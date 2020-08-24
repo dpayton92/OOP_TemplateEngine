03402002fb9be2efd846a096cb0f1f699fac914f
dpayton92 <dpayton92@gmail.com>
2020-08-24 13:09:43 -0700

    updated HTML JS

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,28 @@
+// TODO: Write code to define and export the Employee class
+// create an employee class
+class Employee{
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getname(){
+        return this.name;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    getRole(){
+        return "Employee"
+    }
+}
+// export the employee
+
+module.exports = Employee;
---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,49 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const employee = require("./Employee");
+
+// create a class called Engineer that extends Employee
+class Engineer extends Employee{
+    constructor(name, id, email, github){
+        super(name, id, email)
+        this.github = github;
+        }
+
+    getGithub(){
+        return this.github;
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+}
+// export employee
+createCard() {
+    let theHTML = `
+    <div class="card shadow" style="width: 250px">
+    <div class="card-header bg-primary text-light h2">
+       ${this.name}<br />
+       <span class="h4"><i class="fas fa-glasses"></i> Engineer</span>
+    </div>
+    <div class="card-body" style="background-color: #f1f1f1;">
+       <ul class="list-group">
+          <li class="list-group-item">ID: ${this.id}</li>
+          <li class="list-group-item">
+             Email:
+             <a href="mailto:${this.email}"
+                >${this.email}</a
+             >
+          </li>
+          <li class="list-group-item">
+             GitHub:
+             <a href="https://www.github.com/${this.github}"
+                >${this.github}</a>
+          </li>
+       </ul>
+    </div>
+ </div>`;
+
+    return theHTML;
+ }
+}
+
+module.exports = Engineer;
