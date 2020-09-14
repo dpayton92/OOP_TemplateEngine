053493d87b5745c3c8cd64745eee273b94a16f41
dpayton92 <dpayton92@gmail.com>
2020-09-14 16:30:27 -0700

    made changes and run tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const employee = require("./Employee");
+const Employee = require("./Employee");
 
 // create a class called Engineer that extends Employee
 class Engineer extends Employee{
@@ -16,9 +16,9 @@ class Engineer extends Employee{
         return "Engineer";
     }
 }
-// export employee
-createCard() {
-    let theHTML = `
+export default employee
+createCard() 
+    const theHTML = 
     <div class="card shadow" style="width: 250px">
     <div class="card-header bg-primary text-light h2">
        ${this.name}<br />
@@ -30,20 +30,19 @@ createCard() {
           <li class="list-group-item">
              Email:
              <a href="mailto:${this.email}"
-                >${this.email}</a
-             >
-          </li>
+                >${this.email}
+                </a>
+            </li>
           <li class="list-group-item">
              GitHub:
-             <a href="https://www.github.com/${this.github}"
-                >${this.github}</a>
+             <a href="https://www.github.com/${this.github}">${this.github}
+             </a>
           </li>
        </ul>
     </div>
- </div>`;
+ </div>
+;
+   return theHTML;
 
-    return theHTML;
- }
-}
 
 module.exports = Engineer;
